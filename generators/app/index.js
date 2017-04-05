84662cf30ed930affbec2377875ac6df7348330d
Dylan Nirvana <dylan@dylannirvana.com>
2017-04-05 13:21:34 -0400

    file structure

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,7 +13,7 @@ module.exports = class extends Generator {
     const prompts = [{
       type: 'confirm',
       name: 'someAnswer',
-      message: 'Would you like to enable this option?',
+      message: 'Would you like to scaffold a new application?',
       default: true
     }];
 
@@ -22,15 +22,71 @@ module.exports = class extends Generator {
       this.props = props;
     });
   }
-
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+    config: {
+      // CREATING THE BUILDS FOLDER
+      this.fs.copy(
+        this.templatePath('_builds/_development/_css/_style.css'),
+        this.destinationPath('builds/development/css/style.css')
+      );
+      this.fs.copy(
+        this.templatePath('_builds/_development/_js/_script.js'),
+        this.destinationPath('builds/development/js/script.js')
+      );
+
+      this.fs.copy(
+        this.templatePath('_builds/_production/_css/_style.css'),
+        this.destinationPath('builds/production/css/style.css')
+      );
+      this.fs.copy(
+        this.templatePath('_builds/_production/_js/_script.js'),
+        this.destinationPath('builds/production/js/script.js')
+      );
+      
+      // CREATING THE COMPONENTS FOLDER
+      this.fs.copy(
+        this.templatePath('_components/_scss/_style.scss'),
+        this.destinationPath('components/scss/style.scss')
+      );
+      this.fs.copy(
+        this.templatePath('_components/_scripts/_javascript.js'),
+        this.destinationPath('components/scripts/javascript.js')
+      );
+      // CREATING files
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+      this.fs.copy(
+        this.templatePath('_gulpfile.js'),
+        this.destinationPath('gulpfile.js')
+      );
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_README.md'),
+        this.destinationPath('README.md')
+      );
+      this.fs.copy(
+        this.templatePath('bowerrc'),
+        this.destinationPath('.bowerrc')
+      );
+    } // END config
+  } // END writing
 
   install() {
     this.installDependencies();
+  } // END install
+
+  // Given destination root is ~/projects
+  paths() {
+    this.destinationRoot('project');
+    // returns '~/projects'
+
+    this.destinationPath('project/index.js');
+    // returns '~/projects/index.js'
   }
-};
+
+}; // END generator
