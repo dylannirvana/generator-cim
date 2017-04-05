e300f4d55c9e63e44cb533a0c96c0ba2c9eca32c
Dylan Nirvana <dylan@dylannirvana.com>
2017-04-05 16:19:20 -0400

    files and folders

--- generators/app/index.js
+++ generators/app/index.js
@@ -42,16 +42,52 @@ module.exports = class extends Generator {
         this.templatePath('_builds/_production/_js/_script.js'),
         this.destinationPath('builds/production/js/script.js')
       );
-      
+
       // CREATING THE COMPONENTS FOLDER
       this.fs.copy(
-        this.templatePath('_components/_scss/_style.scss'),
-        this.destinationPath('components/scss/style.scss')
+        this.templatePath('_components/_sass/_modules/__scene0.scss'),
+        this.destinationPath('components/sass/modules/_scene0.scss')
+      );
+      this.fs.copy(
+        this.templatePath('_components/_sass/_modules/__scene1.scss'),
+        this.destinationPath('components/sass/modules/_scene1.scss')
+      );
+      this.fs.copy(
+        this.templatePath('_components/_sass/_modules/__scene2.scss'),
+        this.destinationPath('components/sass/modules/_scene2.scss')
+      );
+      
+      this.fs.copy(
+        this.templatePath('_components/_sass/__base.scss'),
+        this.destinationPath('components/sass/_base.scss')
+      );
+      this.fs.copy(
+        this.templatePath('_components/_sass/__bootstrap.scss'),
+        this.destinationPath('components/sass/_bootstrap.scss')
+      );
+      this.fs.copy(
+        this.templatePath('_components/_sass/__layout.scss'),
+        this.destinationPath('components/sass/_layout.scss')
+      );
+      this.fs.copy(
+        this.templatePath('_components/_sass/__mixins.scss'),
+        this.destinationPath('components/sass/_mixins.scss')
+      );
+      this.fs.copy(
+        this.templatePath('_components/_sass/__variables.scss'),
+        this.destinationPath('components/sass/_variables.scss')
+      );
+
+      this.fs.copy(
+        this.templatePath('_components/_sass/_style.scss'),
+        this.destinationPath('components/sass/style.scss')
       );
       this.fs.copy(
         this.templatePath('_components/_scripts/_javascript.js'),
         this.destinationPath('components/scripts/javascript.js')
       );
+
+
       // CREATING files
       this.fs.copy(
         this.templatePath('_bower.json'),
